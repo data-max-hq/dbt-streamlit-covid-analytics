5bbd30c7e32130607fdeb5b3e94091fca632c1e8
MegiMenalla <menallamegi@gmail.com>
2022-07-04 09:49:39 +0200

    power function

--- demo/models/marts/core/fct_monthly_death_rate.sql
+++ demo/models/marts/core/fct_monthly_death_rate.sql
@@ -3,7 +3,7 @@ with monthly_death_rate as (
     select  b.name,
             a.month_year,
             {{calc_death_rate(n)}} death_rate,
-            power(10) as per_num_of_people
+            power(10,{{n}}) as per_num_of_people
     from {{ ref('stg_deaths_per_month')}} a
     join {{ ref('dim_countries')}} b
     on a.code=b.code
--- demo/models/marts/core/fct_yearly_death_rate.sql
+++ demo/models/marts/core/fct_yearly_death_rate.sql
@@ -1,7 +1,7 @@
 with yearly_death_rate as (
     select  b.name,
             a._year,
-            {{calc_death_rate(4)}} death_rate
+            {{calc_death_rate()}} death_rate
     from {{ ref('stg_deaths_per_year')}} a
     join {{ ref('dim_countries')}} b
     on a.code=b.code
