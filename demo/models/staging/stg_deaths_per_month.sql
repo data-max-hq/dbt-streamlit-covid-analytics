3f372b5a892f4421f31a23e54f914745441fd928
MegiMenalla <menallamegi@gmail.com>
2022-07-04 14:57:04 +0200

    removed objects

--- demo/models/staging/stg_deaths_per_month.sql
+++ demo/models/staging/stg_deaths_per_month.sql
@@ -1,20 +1,20 @@
 {{ config(materialized='incremental',unique_key=['code','month_year']) }}
-
 with stg_deaths_per_month as (
-select code,
-       to_char(date, 'YYYY-MM') as month_year,
-	   sum(new_deaths) deaths
+    select code,
+           to_char(date, 'YYYY-MM') as month_year,
+           sum(new_deaths) deaths
 
-from {{ ref('stg_prepared_source') }}
+    from {{ ref('stg_prepared_source') }} as ps
 
-{% if is_incremental() %}
+    {% if is_incremental() %}
 
-  -- this filter will only be applied on an incremental run
---  where month_year >= (select max(month_year) from {{ this }})
+      -- this filter will only be applied on an incremental run
 
-{% endif %}
+        where ps.date >=  (select max(date) from {{ ref('stg_prepared_source') }} ) - 3
 
-group by code, month_year
+    {% endif %}
+
+    group by code, month_year
 )
 
 select * from stg_deaths_per_month
