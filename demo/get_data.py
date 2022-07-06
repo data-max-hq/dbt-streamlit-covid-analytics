b3ed9237014ef04b05bed0750b863d7ea574dd25
Arbin Bici <arbin.bici@gmail.com>
2022-07-06 15:21:26 +0200

    Add logo and graph

---
+++ demo/get_data.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import pandas as pd
+
+countries = ['AL', 'DE']
+df_main = None
+
+for country_code in countries:
+    url = f"https://corona-api.com/countries/{country_code}?includeTimeline=True"
+    response = requests.request("GET", url)
+    data = json.loads(response.text)
+    timeline = data['data']['timeline']
+    df = pd.DataFrame(timeline)
+    df["name"] = data['data']['name']
+    df["code"] = data['data']['code']
+    df["population"] = data['data']['population']
+    df_main = pd.concat([df_main, df]) if df_main is not None else df
+
+df_main.to_csv(f'seeds/covid_data.csv', index=False)
